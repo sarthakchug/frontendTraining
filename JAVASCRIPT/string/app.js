53f5241ca0a6926ad67e1eea2af571a3f3ab3a4e
sarthakchug <sarthakchug007@gmail.com>
2026-04-28 21:56:19 +0530

    string has been completed

---
+++ JAVASCRIPT/string/app.js
@@ -0,0 +1,76 @@
+                                                    //METHODS
+// console.log("hello world");  //hello world
+
+//type of
+// console.log(typeof val);  //undefined
+// val={}
+// console.log(typeof val);   //object
+// val=[]
+// console.log(typeof val);  //object
+// val="name"
+// console.log(typeof val);    //string
+// val = new String("name")
+// console.log(typeof val);      //object
+// let a ="10";
+// console.log('a: ',typeof a);    //string(ye humsha comma ma hota ha)
+// let c =10;
+// console.log('c: ',typeof c);   //number
+
+// let a="10";
+// let c= 10;
+// console.log(a+c);    // it gives 1010
+// console.log(a-c);    // it gives 0
+// console.log(a/c);    // it gives 1
+// console.log(c==a);      //true  (checks only number)
+// console.log(c===a);     //false  (checks number and type also)
+
+// let name1="sarthak hello good morning"
+// console.log('name1: ', name1);       //name1:  sarthak hello good morning
+
+// let name1="sarthak"
+// let name2="chug"
+// console.log(name1+name2)    //sarthakchug
+// console.log(name1+ " "+name2)  //space->sarthak chug
+// console.log(name1.concat(" ",name2));  //concat->(adds)sarthak chug
+// console.log(`welcome ${name1} ${name2}`);  //Back-Tics `` and interpolation  $ ->sarthak chug
+// console.log(`${2+2} ${name2}`);   //4 chug
+// console.log(name1.length);      //7
+// console.log(name1[name1.length-1]);   //k(last length)
+// console.log(name1[5]);        //a(counts with 0)
+// console.log(name1.at(3));     //t(starts character with index 0)
+// console.log(name1.charAt(3));    //t(starts character with index 0)
+//  console.log(name1.indexOf("good"));   //14
+// console.log(name1.endsWith('morning'));   //true
+
+// let atmpin = "********1224"
+// console.log(atmpin.startsWith('*'));         //true
+
+// console.log(name1.split(" "));     //[ 'sarthak', 'hello', 'good', 'morning' ]
+
+// let email = "sarthakchug007@gmail.com"
+// console.log(email.includes("@"));     //true
+
+//  console.log(name1.slice(8,13));        //hello
+//  console.log(name1.substring(8,13));    //hello
+
+// let aadharCard =512656334423435
+// let updatedAdharCard = "5126"
+// console.log('aadharCard: ', aadharCard);
+// console.log('updatedAdharCard: ', updatedAdharCard.padStart(16,"*"));     //updatedAdharCard:  ************5126
+// console.log('updatedAdharCard: ', updatedAdharCard.padEnd(16,"*"));       //updatedAdharCard:  5126************
+
+// const classname = 'sarthak "chug"'               //classname:  sarthak "chug"
+// const classname = "sarthak 'chug'"               //classname:  sarthak 'chug'
+// const classname = 'sarthak \'chug\' '              //classname:  sarthak 'chug' 
+// console.log('classname: ', classname);
+
+// let name2="              chug            "
+// console.log('name2: ', name2);                //       chug
+// console.log('name2: ', name2.trim());         // chug
+// let finalversion = name2.trim()
+// console.log('finalversion: ', finalversion.length);   //finalversion:  4
+
+
+
+
+
