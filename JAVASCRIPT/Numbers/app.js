660bb1099f6112908ce43b15f57962e0a347c506
sarthakchug <sarthakchug007@gmail.com>
2026-04-30 00:07:15 +0530

    numbers done

---
+++ JAVASCRIPT/Numbers/app.js
@@ -0,0 +1,22 @@
+// console.log(1+2+"3");    //33 
+// console.log("3"+1+2);    //312
+// console.log(9+9+Number("3")+1+2);   //24
+// console.log(typeof(1+2+3));         //number
+// console.log(typeof(1+"2"+3));       //string
+
+// let a="999";
+// console.log(typeof Number(a));  //number
+// console.log(typeof +a);         //number
+// console.log(+"sarthak");        //NaN
+
+let a=10.23455
+// console.log(a.toString(2));                     //1010.0011110000001011011110000000001101000110110111001
+// console.log(a.toFixed(2));                   //10.23
+// console.log(a.toPrecision(4));               //10.23
+// console.log(Number("10.2345 sarthak"));      //NaN
+// console.log(Number.isNaN("123"));               //false
+// console.log(parseInt("10.2345  sarthak"));   //10
+// console.log(parseFloat("10.2345"));          //10.2345
+// console.log("sarthak");                         //sarthak
+
+
