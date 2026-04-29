110f896c032364fd836ae42aa27a4039e1ae8799
sarthakchug <sarthakchug007@gmail.com>
2026-04-30 00:41:00 +0530

    maths done

---
+++ JAVASCRIPT/Mathfunction/app.js
@@ -0,0 +1,14 @@
+        // Pow , round , sqrt , ceil , abs , floor , trunc , Math.min and max , random lie between (0 to 1)
+// console.log(Math.pow(10,2));    //100
+// console.log(Math.sqrt(100));   //10
+// console.log(Math.abs(-20));    //20
+// console.log(Math.max(10,2));   //10
+// console.log(Math.min(10,2));  //2
+
+                              //number to integer
+// let a=10.34567
+// console.log(Math.floor(a));    //10
+// console.log(Math.floor(Math.random()*10));   //1 2 4 0
+// console.log(Math.round(4.4));   //4
+// console.log(Math.ceil(4.9));   //5
+// console.log(Math.trunc(4.8));  //4
