fa62879f6f6767db881cb94b6ef3388af02a7375
sarthakchug <sarthakchug007@gmail.com>
2026-05-01 17:41:41 +0530

    dom and event and project completd

---
+++ JAVASCRIPT/DOM/app.js
@@ -0,0 +1,33 @@
+//how to get elements select by a class name
+// const icondiv =  document.getElementsByClassName("democlass")
+// console.log('icondiv: ', icondiv);
+// const icondiv2 =  document.querySelector(".democlass")
+// console.log('icondiv2: ', icondiv2);
+// const icondiv3 =  document.querySelectorAll(".democlass")
+// console.log('icondiv3: ', icondiv3);
+
+
+// // //how to get elements select by id
+// const icondiv4 =  document.getElementById("democlass")
+// console.log('icondiv4: ', icondiv4);
+// const icondiv5=  document.querySelector("#democlass")
+// console.log('icondiv5: ', icondiv5);
+
+
+// // //how to get elements select by tag
+// const div =  document.getElementsByTagName("div")
+// console.log('div: ', div);
+
+//how to change content->1.inner.text 2.inner.html 3.inner.content
+// const para =document.getElementById("para")
+//     para.innerHTML="<p>sarthak</p>"
+
+
+    //    to view object in vs code
+// const parent=document.querySelector("#parent");
+// const parent = document.getElementById('parent')
+// console.log(parent );
+// console.dir(parent);
+
+    
+
