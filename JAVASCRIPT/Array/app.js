45197811d2a50051fae821c628beaadf5e157d8f
sarthakchug <sarthakchug007@gmail.com>
2026-06-01 10:39:39 +0530

    done

---
+++ JAVASCRIPT/Array/app.js
@@ -0,0 +1,11 @@
+// console.log("hello array");
+
+// let arr = new Array("sar" , "012345" ,"chug")
+// console.log('arr: ', arr);
+
+// let names =["sarthak"]
+// names[0]="chug"
+// console.log('names: ', names);
+
+// let chararr = [ 's' , 'a' , 'r' , 't']
+// console.log('chararr: ', chararr.sort());
